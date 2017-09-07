318861427d3aa4436edaa195ff72235f76456344
Gen Ichino <zenoplex@gmail.com>
2017-09-07 21:02:49 +0900

    feat: setup express

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 // @flow
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { port } = require('./env');
 const { appreciate } = require('./appreciate');
 
-const username = 'suzuki-y';
+const app = express();
+app
+  .use(cors())
+  .use(bodyParser.json())
+  // $FlowFixMe post doesn't exist?
+  .post('/', (req, res) => {
+    const data = JSON.stringify(req.body, null, 2);
+    console.log(data);
 
-appreciate({ username });
+    appreciate({ username: 'suzuki-y' })
+      .then(() => {
+        res.status(200);
+        res.end();
+      })
+      .catch(() => {
+        res.status(422);
+        res.end();
+      });
+  })
+  .listen(port, () => {
+    console.log(`express server started at ${port}`);
+  });
