005a5be818f77855c59d77b2ec53c7db39e4a241
Gen Ichino <zenoplex@gmail.com>
2017-09-07 23:43:02 +0900

    refactor: add args

--- src/appreciate.js
+++ src/appreciate.js
@@ -18,7 +18,11 @@ type AppreciateOptions = {
 const appreciate: AppreciateOptions => Promise<void>
 = async ({ username, point = 1, hashtag = 'thanks' }) => {
   // $FlowFixMe not sure why await returns Promise
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+  ],
+  });
   const page = await browser.newPage();
 
   return R.pipeP(
