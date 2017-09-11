f98f475e2a1824d85a90d2098ace412691e00b8e
Gen Ichino <zenoplex@gmail.com>
2017-09-11 22:11:39 +0900

    feat: add nodeEnv to env

--- src/env.js
+++ src/env.js
@@ -1,13 +1,15 @@
 // @flow
 require('dotenv').config();
 
-const { AUTH, PORT } = process.env;
+const { AUTH, PORT, NODE_ENV } = process.env;
 
 // AUTH expect foo:bar
 const auth = AUTH ? AUTH.split(':') : undefined;
 const port = PORT || 3000;
+const nodeEnv = NODE_ENV || 'dev';
 
 module.exports = {
   auth,
   port,
+  nodeEnv,
 };
