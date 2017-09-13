ce8521c39a4dd6b7e51b710f978e10ff3dc8cb98
Gen Ichino <zenoplex@gmail.com>
2017-09-14 02:34:17 +0900

    refactor: rewrite express

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // @flow
 const express = require('express');
 const bodyParser = require('body-parser');
-// const basicAuth = require('express-basic-auth');
+const basicAuth = require('express-basic-auth');
 const cors = require('cors');
 const { auth, port } = require('./env');
 const { appreciate } = require('./appreciate');
@@ -9,26 +9,40 @@ const { test } = require('./test');
 
 const app = express();
 
+if (auth) {
+  app.use(
+    basicAuth({
+      users: {
+        [auth[0]]: auth[1],
+      },
+    }),
+  );
+}
+
 app
   .use(cors())
   .use(bodyParser.json())
   // $FlowFixMe post doesn't exist?
-  .post('/', (req, res) => {
-    test().then(() => {
+  .post('/', async (req, res) => {
+    try {
+      await test();
       res.json({ status: 'success' });
-    });
+    } catch (error) {
+      res.status(422);
+      res.json({ status: 'error', message: error.message });
+      process.exit(1);
+    }
   })
-  // $FlowFixMe post doesn't exist?
-  .post('/unipos', (req, res) => {
-    appreciate(req.body)
-      .then(() => {
-        res.status(200);
-        res.json({ status: 'success' });
-      })
-      .catch((error) => {
-        res.status(422);
-        res.json({ status: 'error', message: error.message });
-      });
+  .post('/unipos', async (req, res) => {
+    try {
+      await appreciate(req.body);
+      res.status(200);
+      res.json({ status: 'success' });
+    } catch (error) {
+      res.status(422);
+      res.json({ status: 'error', message: error.message });
+      process.exit(1);
+    }
   })
   .listen(port, () => {
     console.log(`express server started at ${port}`);
